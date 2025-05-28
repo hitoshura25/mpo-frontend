ee1ec9254eb84c05ad24d5b5c636b800a637b851
Vinayak Menon <hitoshura25@users.noreply.github.com>
2025-05-27 19:51:34 -0500

    Trying out blank value for publicPath (#7)

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/[name].bundle.js',
-    publicPath: 'auto'
+    publicPath: ''
   },
   module: {
     rules: [
